276cba21556e326bbc8aeec9bfa36f1b4af49712
ウエスリー <wesley@cipolla.evokateur.net>
2025-08-02 01:25:58 -0700

    maximum verbosity in setup.sh

--- jobhunting-crew/setup.sh
+++ jobhunting-crew/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo "[Re]creating virtual environment and installing dependencies.."
 python -m venv .venv --clear
 source .venv/bin/activate
 pip install -r requirements.txt
+
+echo "Installing Jupyter kernel spec for $(pwd)/.venv.."
 python -m ipykernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=jobhunting-crew --display-name "Jobhunting Crew"
+
+echo "Use kernel 'jobhunting-crew' in Jupyter notebooks."
